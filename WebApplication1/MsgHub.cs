7c38683ba84f3179c8af6740f4d760e920919257
y18830899257@126.com <y18830899257@126.com>
2019-08-06 11:54:54 +0800

    添加项目文件。

---
+++ WebApplication1/MsgHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebApplication1
+{
+    public class MsgHub : Hub
+    {
+        public void Hello(string name,string msg)
+        {
+            Clients.All.getmsg(name,msg);
+        }
+    }
+}
